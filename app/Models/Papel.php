f4af5d7f2d1c0002466119c2683c2236d0553823
BASDiniz <brunodiniz1818@gmail.com>
2021-02-21 13:24:04 -0300

    Criando model Papel e fazendo o relacionamento com AtividadeUsuario

--- app/Models/Papel.php
+++ app/Models/Papel.php
@@ -5,11 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class AtividadeUsuario extends Model
+class Papel extends Model
 {
     use HasFactory;
-
-    public function papel(){
-        return $this->hasOne('App\Models\Papel');
-    }
 }
