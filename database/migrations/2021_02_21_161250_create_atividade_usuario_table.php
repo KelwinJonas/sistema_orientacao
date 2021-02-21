2273fc362ce3ac0393df3990be5bd64d32086530
BASDiniz <brunodiniz1818@gmail.com>
2021-02-21 13:15:31 -0300

    Adicionando tabela de AtividadeUsuario

---
+++ database/migrations/2021_02_21_161250_create_atividade_usuario_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAtividadeUsuarioTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('atividade_usuario', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('usuario_id');
+            $table->foreign('usuario_id')->references('id')->on('usuario');
+
+            $table->unsignedBigInteger('atividade_academica_id');
+            $table->foreign('atividade_academica_id')->references('id')->on('atividade_academica');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('atividade_usuario');
+    }
+}
