43d148fc7283e5ed7579c5ccc5f19f7c4b05ea73
BASDiniz <brunodiniz1818@gmail.com>
2021-02-21 13:59:22 -0300

    Fazendo o relacionamento Anotacao com Anotacao

--- app/Models/Anotacao.php
+++ app/Models/Anotacao.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Anotacao extends Model
 {
     use HasFactory;
+
+    public function anotacoes(){
+        return $this->hasMany('App\Models\Anotacao');
+    }
 }
