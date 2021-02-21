5ca638522127dc9e78a6be9a9e03c1aa7ee858f2
BASDiniz <brunodiniz1818@gmail.com>
2021-02-21 13:58:06 -0300

    Criando model Anotacao e fazendo o relacionamento com Campo

--- app/Models/Anotacao.php
+++ app/Models/Anotacao.php
@@ -5,11 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Campo extends Model
+class Anotacao extends Model
 {
     use HasFactory;
-
-    public function anotacoes(){
-        return $this->hasMany('App\Models\Anotacao');
-    }
 }
