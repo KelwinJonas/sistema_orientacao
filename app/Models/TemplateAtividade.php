ee5a7f627e6fba18761eda93977f986bb0ac0315
BASDiniz <brunodiniz1818@gmail.com>
2021-02-21 13:45:26 -0300

    Fazendo o relacionamento de TemplateAtividade com Secao

--- app/Models/TemplateAtividade.php
+++ app/Models/TemplateAtividade.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class TemplateAtividade extends Model
 {
     use HasFactory;
+
+    public function secoes(){
+        return $this->hasMany('App\Models\Secao');
+    }
 }
