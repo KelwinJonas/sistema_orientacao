b21c71ec6aab0dac5ef070de12a73ba21c4a2fcd
BASDiniz <brunodiniz1818@gmail.com>
2021-02-21 13:19:24 -0300

    Criando model AtividadeAcademica e fazendo o relacionamento com AtividadeUsuario

---
+++ app/Models/AtividadeAcademica.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class AtividadeAcademica extends Model
+{
+    use HasFactory;
+
+    public function atividadeUsuario(){
+        return $this->hasMany('App\Models\AtividadeUsuario');
+    }
+}
