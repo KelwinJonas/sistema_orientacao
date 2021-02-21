b54cb412df0879cd877d5a839d01aa4e135aa59c
BASDiniz <brunodiniz1818@gmail.com>
2021-02-21 13:09:01 -0300

    Adicionando tabela de AtividadeAcademica

---
+++ database/migrations/2021_02_21_160619_create_atividade_academica_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAtividadeAcademicaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('atividade_academica', function (Blueprint $table) {
+            $table->id();
+            $table->string('tipo');
+            $table->string('titulo');
+            $table->date('data_inicio');
+            $table->date('data_fim');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('atividade_academica');
+    }
+}
