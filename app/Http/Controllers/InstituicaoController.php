f1627f54e3bbe1cd3914450a081a3254e1dcbd3c
BASDiniz <brunodiniz1818@gmail.com>
2021-02-22 22:42:35 -0300

    Adicionando validaçao no cadastro de uma instituiçao

--- app/Http/Controllers/InstituicaoController.php
+++ app/Http/Controllers/InstituicaoController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Instituicao;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class InstituicaoController extends Controller
 {
@@ -13,6 +14,17 @@ class InstituicaoController extends Controller
 
     public function salvarCadastrarInstituicao(Request $request){
         $entrada = $request->all();
+
+        $messages = [
+            'required' => 'O campo nome é obrigatório.',
+            'min' => 'O campo :attribute deve ter no mínimo :min caracteres.',
+            'max' => 'O campo :attribute deve ter no máximo :max caracteres.',
+        ];
+
+        $validator = Validator::make($entrada, Instituicao::$rules, $messages);
+        if($validator->fails()){
+            return redirect()->back()->withErrors($validator)->withInput();
+        }
 
         $instituicao = new Instituicao;
         $instituicao->nome = $entrada['nome'];
