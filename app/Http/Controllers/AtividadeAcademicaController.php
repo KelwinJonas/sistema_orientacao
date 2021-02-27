8ba21f8d87e901d47dadaa393149955f37851b4f
BASDiniz <brunodiniz1818@gmail.com>
2021-02-27 18:40:16 -0300

    Adicionando validaçao na criaçao de uma atividade

--- app/Http/Controllers/AtividadeAcademicaController.php
+++ app/Http/Controllers/AtividadeAcademicaController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\AtividadeAcademica;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class AtividadeAcademicaController extends Controller
 {
@@ -14,6 +15,18 @@ class AtividadeAcademicaController extends Controller
     public function salvarCadastrarAtividade(Request $request){
         $entrada = $request->all();
         
+        $messages = [
+            'required' => 'O campo :attribute é obrigatório.',
+            'min' => 'O campo :attribute deve ter no mínimo :min caracteres.',
+            'max' => 'O campo :attribute deve ter no máximo :max caracteres.',
+            'date' => 'O campo :attribute está inválido'
+        ];
+
+        $validator = Validator::make($entrada, AtividadeAcademica::$rules, $messages);
+        if($validator->fails()){
+            return redirect()->back()->withErrors($validator)->withInput();
+        }
+
         $atividadeAcademica = new AtividadeAcademica();
         $atividadeAcademica->tipo = $entrada['tipo'];
         $atividadeAcademica->titulo = $entrada['titulo'];
