40db012f44847fbcb00a065fd8892d64907bf135
BASDiniz <brunodiniz1818@gmail.com>
2021-04-02 11:18:46 -0300

    Refazendo footer

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-light bg-light" style="">
     <div class="navbar-header">
-        <a href="#" class="navbar-brand">
-            <img id="logo-navbar" src="{{asset('images/logo_bussola_1.png')}}" alt="">
+        <a href="#" class="navbar-brand" id="cor-texto-logo-navbar">
+            <img id="logo-navbar" src="{{asset("images/logo_bussola_1.png")}}" alt="">
             Orientação
         </a>
     </div>
