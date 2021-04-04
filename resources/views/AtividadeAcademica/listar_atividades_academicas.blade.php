7620865f43bc8e486beb7d4ded86b6b4bb68a371
BASDiniz <brunodiniz1818@gmail.com>
2021-04-03 22:22:44 -0300

    Tela apos login - listar atividades

--- resources/views/AtividadeAcademica/listar_atividades_academicas.blade.php
+++ resources/views/AtividadeAcademica/listar_atividades_academicas.blade.php
@@ -1,7 +1,35 @@
 @extends('layouts.app')
 
 @section('content')
-    <br>
+    <div class="container-main">
+        <div class="container">
+            <div class="col-md-12" id="cabecalho-listar-atividades">Minhas atividades</div>
+            <div id="div-lista-de-atividades">
+                <div class="row">
+                    @foreach ($atividades as $atividade)
+                        <div class="style_card_tema">
+                            <div class="container div-conteudo-card">
+                                <div class="row">
+                                    <div class="col-md-12 div-titulo-card">
+                                        <a class="link-titulo-atividade" href="#">{{$atividade->titulo}}</a>
+                                    </div>
+                                    <div class="col-md-12">
+                                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a...
+                                    </div>
+                                </div>
+                                <hr>
+                                <div class="col-md-12 div-usuario">
+                                    <img src="{{asset('images/logo_user_default.png')}}" alt="Orientação" width="35px"> 
+                                    <span style="font-size: 15px;">Fulano</span>
+                                </div>
+                            </div>
+                        </div>
+                    @endforeach
+                </div>
+            </div>
+        </div>
+    </div>
+    {{-- <br>
     <h2>Minhas atividades</h2>
 
     <div style="overflow: auto; height: 450px">
@@ -25,5 +53,5 @@
                 @endforeach
             </tbody>
         </table>
-    </div>
+    </div> --}}
 @endsection
